7dc3ce5f82f99cd4dd7ab3dd7a73c486bba57041
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-07-30 14:15:57 -0300

    exercicio

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogJson: '',
+    };
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.dogJson.message.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("dogURL", this.state.data.message);
+    const dogBreed = this.state.dogJson.message.split('/')[4];
+    alert(dogBreed);
+  }
+
+  fetchDog() {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    fetch(url)
+      .then((dado) => dado.json())
+      .then((data) => this.state({ dogJson: data }));
+  }
+
+  render() {
+    if (this.state.dogJson === '') return 'loading...';
+    return (
+      <div>
+        <p>Doguinhos</p>
+        <button type="button" onClick={ this.fetchDog }>Novo doguinho!</button>
+        <div>
+          <img src={ this.state.dogJson.message } alt="Random dog" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
